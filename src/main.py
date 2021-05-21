a80c38611c496cccb62c908286b64ca9539ff4b2
Cyzorz <cyzorzyes@gmail.com>
2021-05-21 10:59:58 -0700

    fix request limit

--- src/main.py
+++ src/main.py
@@ -1,23 +1,35 @@
 import requests
 import json
 import prettytable
-headers = {'Content-type': 'application/json'}
-data = json.dumps({"seriesid": ['CUUR0000SA0','SUUR0000SA0'],"startyear":"2010", "endyear":"2021"})
-p = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
-json_data = json.loads(p.text)
-for series in json_data['Results']['series']:
-    x=prettytable.PrettyTable(["series id","year","period","value","footnotes"])
-    seriesId = series['seriesID']
-    for item in series['data']:
-        year = item['year']
-        period = item['period']
-        value = item['value']
-        footnotes=""
-        for footnote in item['footnotes']:
-            if footnote:
-                footnotes = footnotes + footnote['text'] + ','
-        if 'M01' <= period <= 'M12':
-            x.add_row([seriesId,year,period,value,footnotes[0:-1]])
-    output = open(seriesId + '.txt','w')
-    output.write (x.get_string())
-    output.close()
+
+class Unemployment:
+    def __init__(self, startYear, endYear):
+        headers = {'Content-type': 'application/json'}
+        data = json.dumps({"seriesid": ['CUUR0000SA0','SUUR0000SA0'],"startyear":f"{str(startYear)}", "endyear":f"{str(endYear)}"})
+        p = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
+        self.json_data = json.loads(p.text)
+
+    def load(self):
+        try:
+            for dataSeries in self.json_data['Results']['series']:
+                    x=prettytable.PrettyTable(["series id","year","period","value","footnotes"])
+                    seriesId = dataSeries['seriesID']
+                    for item in dataSeries['data']:
+                        year = item['year']
+                        period = item['period']
+                        value = item['value']
+                        footnotes=""
+                        for footnote in item['footnotes']:
+                            if footnote:
+                                footnotes = footnotes + footnote['text'] + ','
+                        if 'M01' <= period <= 'M12':
+                            x.add_row([seriesId,year,period,value,footnotes[0:-1]])
+        except KeyError:
+            print("Ignoring daily request limit and displaying previous query results:")
+            return
+
+        output = open(seriesId + '.txt','w')
+        output.write (x.get_string())
+        output.close()
+
+Unemployment(2019, 2020).load()
