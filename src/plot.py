fd7db6ce77b09e07f03542f855415e79056445f5
Cyzorz <cyzorzyes@gmail.com>
2021-05-21 20:21:13 -0700

    cleanup

--- src/plot.py
+++ src/plot.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-import matplotlib.cbook as cbook
+import matplotlib.dates as mdates
 
 import numpy as np
 import pandas as pd
@@ -20,12 +20,25 @@ class Plot:
     def get_file(self):
         return self.dVal
 
-    def plot(self, x, y, xMax, yMax, xMin=0, yMin=0):
-        plt.scatter(x, y)
+    def date_plot(self, xMax, yMax, xMin=0, yMin=0):
         plt.gcf().autofmt_xdate()
         plt.gca().set_xlim(left=xMin, right=xMax)
         plt.gca().set_ylim(bottom=yMin, top=yMax)
+
+    def add_style(self, style):
+        try:
+            plt.style.use(style)
+        except FileNotFoundError and OSError:
+            print("ERROR: Invalid Style!")
+            return
+
+    def scatter(self, x, y):
+        plt.scatter(x, y)
         plt.grid(True)
-    
+
+    def plot(self, x, y):
+        plt.plot(x, y)  
+        plt.grid(True)
+
     def render_plot(self):
         return plt.show()
